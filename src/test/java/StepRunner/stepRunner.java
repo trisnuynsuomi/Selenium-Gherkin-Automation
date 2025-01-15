1dc5b2cbb4abc3491cb8732c3f4c635b21585af4
trisnuynsuomi <minhtri11066100@gmail.com>
2025-01-15 17:05:13 +0700

    add config test file

--- src/test/java/StepRunner/stepRunner.java
+++ src/test/java/StepRunner/stepRunner.java
@@ -4,6 +4,6 @@ package StepRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/feature", glue = {"stepDefinitions"})
+@CucumberOptions(features = "src/test/java/feature", glue = {"stepDefinitions"}, plugin = {"pretty", "html:target/cucumber-reports.html"})
 public class stepRunner extends AbstractTestNGCucumberTests {
 }
